8706d5400e2a50b74223b5c490b0f6a43d794843
MacJ13 <jacdim02@gmail.com>
2023-08-03 23:48:28 +0200

    Create Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./style/style.css";
 import Header from "./components/Header";
-
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
