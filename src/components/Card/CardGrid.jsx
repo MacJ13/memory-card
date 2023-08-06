48e1021b0541dad0fea342f6cbfa7ab05896f503
MacJ13 <jacdim02@gmail.com>
2023-08-06 17:46:44 +0200

    Get data in CardGrid with useFetch

--- src/components/Card/CardGrid.jsx
+++ src/components/Card/CardGrid.jsx
@@ -1,8 +1,29 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-const CardGrid = ({ children }) => {
+
+import { useFetch } from "../useFetch";
+import Card from "./Card";
+import CardLoading from "./CardLoading";
+
+const CardGrid = ({ onCardClick }) => {
+  const [data, error] = useFetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${4}&offset=${0}`
+  );
+
+  const renderCards = () => {
+    return data.map((pokemon) => {
+      return (
+        <Card key={pokemon.name} pokemon={pokemon} onCardClick={onCardClick} />
+      );
+    });
+  };
   return (
     <>
-      <div className="card-grid">{children}</div>;
+      {data.length === 0 && !error ? (
+        <CardLoading />
+      ) : (
+        <div className="card-grid">{renderCards()}</div>
+      )}
     </>
   );
 };
