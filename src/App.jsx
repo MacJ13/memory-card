9dcd0f837b51f577351414772fe416789af1683e
MacJ13 <jacdim02@gmail.com>
2023-08-11 12:17:21 +0200

    Remove style.css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-// import "./style/style.css";
 import "./style/main.scss";
-// import Header from "./components/Header";
+
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 function App() {
