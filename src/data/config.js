5b69075e0f6322e8b12994b5bc85be2d3fd2ee96
MacJ13 <jacdim02@gmail.com>
2023-10-08 00:15:52 +0200

    add data of objects of difficulty levels

--- src/data/config.js
+++ src/data/config.js
@@ -14,9 +14,16 @@ const LEVEL = 1;
 export const initialGame = {
   status: STATUS.start,
   level: LEVEL,
+  difficulty: "",
   currentScore: 0,
   highScore: 0,
 };
+
+export const LEVEL_DIFFICULTIES = [
+  { name: "easy", count: 4 },
+  { name: "medium", count: 6 },
+  { name: "hard", count: 8 },
+];
 
 export const calculateLimit = (level) => {
   return 4 * level >= 16 ? 16 : 4 * level;
