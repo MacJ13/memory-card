41433a4cc432476d83a1331fc07d5b5781037924
MacJ13 <jacdim02@gmail.com>
2023-10-10 20:03:06 +0200

    Create initial-game-setup object

--- src/data/config.js
+++ src/data/config.js
@@ -11,19 +11,19 @@ export const STATUS = {
 
 const LEVEL = 1;
 
-export const initialGame = {
-  status: STATUS.start,
-  level: LEVEL,
-  difficulty: "",
-  currentScore: 0,
-  highScore: 0,
-};
-
 export const LEVEL_DIFFICULTIES = [
   { name: "easy", count: 4 },
   { name: "medium", count: 8 },
   { name: "hard", count: 12 },
 ];
+
+export const INITIAL_GAME_SETUP = {
+  status: STATUS.start,
+  level: LEVEL,
+  limit: LEVEL_DIFFICULTIES.count,
+  currentScore: 0,
+  highScore: 0,
+};
 
 export const calculateLimit = (level) => {
   return 4 * level >= 16 ? 16 : 4 * level;
