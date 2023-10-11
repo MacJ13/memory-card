47badc3c086955dbc63a959bfd6a567d48a87351
MacJ13 <jacdim02@gmail.com>
2023-10-11 12:46:18 +0200

    Create and add ModalBox components

---
+++ src/components/Modal/ModalBox.jsx
@@ -0,0 +1,4 @@
+/* eslint-disable react/prop-types */
+const ModalBox = ({ children }) => <div className="modal-box">{children}</div>;
+
+export default ModalBox;
--- src/components/Modal/ModalNextLevel.jsx
+++ src/components/Modal/ModalNextLevel.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import Button from "../Button";
+import ModalBox from "./ModalBox";
 
 // eslint-disable-next-line react/prop-types
 const ModalNextLevel = ({
@@ -10,21 +11,23 @@ const ModalNextLevel = ({
 }) => {
   return (
     <>
-      <div className="modal-start">
-        <h3 className="h2">You pass the level {level}!</h3>
-        <p className="paragraph">
-          Your current result is: <strong>{currentScore}</strong>.
-          {/* {highScore !== 0 && currentScore + 1 >= highScore ? (
+      <ModalBox>
+        <div className="modal-start">
+          <h3 className="h2">You pass the level {level}!</h3>
+          <p className="paragraph">
+            Your current result is: <strong>{currentScore}</strong>.
+            {/* {highScore !== 0 && currentScore + 1 >= highScore ? (
             <em> You also set new record score!</em>
           ) : null} */}
-        </p>
-        <Button className="modal-btn" onClick={onHandleClick}>
-          Next level
-        </Button>
-        <Button className="modal-btn" onClick={onQuitClick}>
-          Quit
-        </Button>
-      </div>
+          </p>
+          <Button className="modal-btn" onClick={onHandleClick}>
+            Next level
+          </Button>
+          <Button className="modal-btn" onClick={onQuitClick}>
+            Quit
+          </Button>
+        </div>
+      </ModalBox>
     </>
   );
 };
