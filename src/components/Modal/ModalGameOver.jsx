9ddc1c868d137ef97b56d7e8359f2c9d56b6e410
MacJ13 <jacdim02@gmail.com>
2023-10-11 12:51:22 +0200

    Add modal-box class selector

--- src/components/Modal/ModalGameOver.jsx
+++ src/components/Modal/ModalGameOver.jsx
@@ -7,25 +7,23 @@ const ModalGameOver = ({ onRestartClick, onQuitClick, currentScore }) => {
   return (
     <>
       <ModalBox>
-        <div className="modal-start">
-          <h1 className="h2">Game Over!</h1>
-          <p className="paragraph">
-            Your current result is:{" "}
-            <strong style={{ fontFamily: '"Cursed Timer", sans-serif' }}>
-              {currentScore}
-            </strong>
-            <br />
-            {/* {highScore !== 0 && currentScore > highScore + 1 ? (
+        <h1 className="h2">Game Over!</h1>
+        <p className="paragraph">
+          Your current result is:{" "}
+          <strong style={{ fontFamily: '"Cursed Timer", sans-serif' }}>
+            {currentScore}
+          </strong>
+          <br />
+          {/* {highScore !== 0 && currentScore > highScore + 1 ? (
             <em> You also set new record score!</em>
           ) : null} */}
-          </p>
-          <Button className="modal-btn" onClick={onRestartClick}>
-            Restart game
-          </Button>
-          <Button className="modal-btn" onClick={onQuitClick}>
-            Quit
-          </Button>
-        </div>
+        </p>
+        <Button className="modal-btn" onClick={onRestartClick}>
+          Restart game
+        </Button>
+        <Button className="modal-btn" onClick={onQuitClick}>
+          Quit
+        </Button>
       </ModalBox>
     </>
   );
